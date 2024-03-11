cf28657abab8891121c1cfd16a4c8212e38a52a1
Braian Ferreyra <braianezequielferreyra39936964@gmail.com>
2024-03-11 23:26:21 +0100

    GX2-2658 Forth commit

--- src/test/java/e2e/steps/Elements/GX3_2658_TextBox.java
+++ src/test/java/e2e/steps/Elements/GX3_2658_TextBox.java
@@ -79,6 +79,30 @@ public class GX3_2658_TextBox extends TestBase {
 				"El campo de nombre completo no contiene el valor esperado.");
 	}
 
+	@Test
+	@Severity(NORMAL)
+	@Issue("https://upexgalaxy38.atlassian.net/browse/GX3-2658")
+	@Description("Este caso de prueba va a validar que el usuario pueda rellenar el campo 'Current Address'")
+	@DisplayName("2659 | TC03: Validar rellenar el campo 'Current Address' con datos correctos")
+	public void TC03_CurrentAddress() throws InterruptedException {
+
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		chromeOptions.addArguments("--window-size=1920,1080");
+
+		web = new ChromeDriver(chromeOptions);
+		web.get("https://demoqa.com/text-box");
+
+		WebElement campoCurrentAddress = web.findElement(By.id("currentAddress"));
+
+		String textoIngresado = "Carrer Masarbones, 8";
+		campoCurrentAddress.sendKeys(textoIngresado);
+
+		String valorCampoCurrentAddress = campoCurrentAddress.getAttribute("value");
+		assertEquals(valorCampoCurrentAddress, textoIngresado,
+				"El campo de dirección actual no contiene el valor esperado.");
+	}
+
 	@AfterClass
 	public void tearDown() {
 		web.quit();
